ed8aaf9e804fd1dc198fd4b41c243030331c5f8e
Bodhisha Thomas <bodhisha@gmail.com>
2019-09-11 13:55:36 -0400

    Create Day0.c

---
+++ Day0.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main() {
+    char input[105]; 
+    scanf("%[^\n]",input); 
+    printf("Hello, World.\n");
+    printf("%s", input);
+    return 0;
+}
